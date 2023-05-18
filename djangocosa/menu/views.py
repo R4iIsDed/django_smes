30bc1d7b41aec9d2a1a63d53f5335cdaee3a6151
R4iIsDed <127206499+R4iIsDed@users.noreply.github.com>
2023-05-18 13:53:02 -0400

    agregado views y urls completo

--- djangocosa/menu/views.py
+++ djangocosa/menu/views.py
@@ -9,3 +9,159 @@ def create (request) :
 
 def index (request) :
     return render (request, 'menu/index.html');
+
+def agregar_producto(request):
+    return render(request, 'menu/agregar_producto.html');
+
+def banadmin(request):
+    return render(request, 'menu/banadmin.html');
+
+def borrarcuenta(request):
+    return render(request, 'menu/borrarcuenta.html');
+
+def Cactus(request):
+    return render(request, 'menu/Cactus.html');
+
+def Carrito(request):
+    return render(request, 'menu/Carrito.html');
+
+def categorias(request):
+    return render(request, 'menu/categorias.html');
+
+def changeforgoh(request):
+    return render(request, 'menu/changeforgoh.html');
+
+def chNGE(request):
+    return render(request, 'menu/chNGE.html');
+
+def compra_cactus1(request):
+    return render(request, 'menu/compra_cactus1.html');
+
+def compra_cactus2(request):
+    return render(request, 'menu/compra_cactus2.html');
+
+def compra_cactus3(request):
+    return render(request, 'menu/compra_cactus3.html');
+
+def compra_cactus4(request):
+    return render(request, 'menu/compra_cactus4.html');
+
+def compra_cactus5(request):
+    return render(request, 'menu/compra_cactus5.html');
+
+def compra_cactus6(request):
+    return render(request, 'menu/compra_cactus6.html');
+
+def Compra_fertilizante1(request):
+    return render(request, 'menu/Compra_fertilizante1.html');
+
+def Compra_fertilizante2(request):
+    return render(request, 'menu/Compra_fertilizante2.html');
+
+def Compra_fertilizante3(request):
+    return render(request, 'menu/Compra_fertilizante3.html');
+
+def Compra_fertilizante4(request):
+    return render(request, 'menu/Compra_fertilizante4.html');
+
+def Compra_fertilizante5(request):
+    return render(request, 'menu/Compra_fertilizante5.html');
+
+def Compra_fertilizante6(request):
+    return render(request, 'menu/Compra_fertilizante6.html');
+
+def Compra_maceta1(request):
+    return render(request, 'menu/Compra_maceta1.html');
+
+def Compra_maceta2(request):
+    return render(request, 'menu/Compra_maceta2.html');
+
+def Compra_maceta3(request):
+    return render(request, 'menu/Compra_maceta3.html');
+
+def Compra_maceta4(request):
+    return render(request, 'menu/Compra_maceta4.html');
+
+def Compra_maceta5(request):
+    return render(request, 'menu/Compra_maceta5.html');
+
+def Compra_maceta6(request):
+    return render(request, 'menu/Compra_maceta6.html');
+
+def Compra_Pago(request):
+    return render(request, 'menu/Compra_Pago.html');
+
+def Compra_pesticida1(request):
+    return render(request, 'menu/Compra_pesticida1.html');
+
+def Compra_pesticida2(request):
+    return render(request, 'menu/Compra_pesticida2.html');
+
+def Compra_pesticida3(request):
+    return render(request, 'menu/Compra_pesticida3.html');
+
+def compra_planta1(request):
+    return render(request, 'menu/compra_planta1.html');
+
+def Compra_planta2(request):
+    return render(request, 'menu/Compra_planta2.html');
+
+def Compra_planta3(request):
+    return render(request, 'menu/Compra_planta3.html');
+
+def Compra_planta4(request):
+    return render(request, 'menu/Compra_planta4.html');
+
+def Compra_planta5(request):
+    return render(request, 'menu/Compra_planta5.html');
+
+def Compra_planta6(request):
+    return render(request, 'menu/Compra_planta6.html');
+
+def Contacto(request):
+    return render(request, 'menu/Contacto.html');
+
+def editar_producto(request):
+    return render(request, 'menu/editar_producto.html');
+
+def eliminar_producto(request):
+    return render(request, 'menu/eliminar_producto.html');
+
+def Fertilizante(request):
+    return render(request, 'menu/Fertilizante.html');
+
+def Flores(request):
+    return render(request, 'menu/Flores.html');
+
+def Maceteros(request):
+    return render(request, 'menu/Maceteros.html');
+
+def oferta1(request):
+    return render(request, 'menu/oferta1.html');
+
+def oferta2(request):
+    return render(request, 'menu/oferta2.html');
+
+def oferta3(request):
+    return render(request, 'menu/oferta3.html');
+
+def oferta4(request):
+    return render(request, 'menu/oferta4.html');
+
+def oferta5(request):
+    return render(request, 'menu/oferta5.html');
+
+def oferta6(request):
+    return render(request, 'menu/oferta6.html');
+
+def ofertas(request):
+    return render(request, 'menu/ofertas.html');
+
+def Perfil_administrador(request):
+    return render(request, 'menu/Perfil_administrador.html');
+
+def Pesticidas(request):
+    return render(request, 'menu/Pesticidas.html');
+
+def profile(request):
+    return render(request, 'menu/profile.html');
