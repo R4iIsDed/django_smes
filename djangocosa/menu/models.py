7b35bfcb2af55acbe7a34562297ac603b7403ebc
EgoSunLuxMundy <128147968+EgoSunLuxMundy@users.noreply.github.com>
2023-05-19 10:26:39 -0400

    a
    .

--- djangocosa/menu/models.py
+++ djangocosa/menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tabla1(models):
+    codigoTabla1= models.AutoField(primary_key=True)
+    nombre = models.AutoField(max_length=20)
+    
+class tabla2(models):
+    codigoTabla2= models.AutoField(primary_key=True)
+    nombre2 = models.AutoField(max_length=30,null= True,blank=False)
+    edad = models.IntegerField()
+    Foto = models.ImageField(upload_to="tabla2_images")
+    tabla1 = models.ForeignKey(Tabla1,on_delete=models.CASCADE)
