8ab19c2dab49469e091bbe6d70b5fd7be8283a9d
MrBigHD <ge.yanez@duocuc.cl>
2023-06-09 01:38:54 -0400

    a

--- djangocosa/djangocosa/settings.py
+++ djangocosa/djangocosa/settings.py
@@ -133,3 +133,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
